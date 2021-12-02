6b279bda04ac3ff997b7383d93df5b538dda4d6e
Silvia Kuzmova <silvia.kuzmova21@gmail.com>
2021-12-02 17:47:13 +0100

    create new project

---
+++ BitpandaChallenge/BitpandaChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitpandaChallenge
+//
+//  Created by Silvia Kuzmova on 02.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+
+}
+
